bb175ba5d64d84523d969c6df642d7899e1f55c6
vbl10 <99830114+vbl10@users.noreply.github.com>
2024-10-22 16:32:25 -0300

    Usar interrupções para medição mais precisa

--- ControleMotorDC.ino
+++ ControleMotorDC.ino
@@ -1,3 +1,9 @@
+#include <TimerOne.h>
+
+const int nLeituras = 1000;
+const int periodoAmostragemUs = 1000;
+const int atrasoAplicacaoSinalReferenciaUs = 10000;
+
 void definirDirecao(bool horario) {
   digitalWrite(3, horario);
   digitalWrite(4, !horario);
@@ -17,6 +23,13 @@ int lerSaidaSistema() {
   return analogRead(A0);
 }
 
+int nLeituraAtual = 0;
+void imprimirLeitura() {
+  if (nLeituraAtual >= nLeituras) return;
+  Serial.println(lerSaidaSistema());
+  nLeituraAtual++;
+}
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(2,OUTPUT);                 // STBY TB6612
@@ -27,10 +40,12 @@ void setup() {
   digitalWrite(2,HIGH);              // Coloca o driver TB6612 em operação
   definirDirecao(true);                         // Seta direção de acordo com o valor de "dir"
   Serial.begin(115200);              // Serial initialize
+  
+  Timer1.initialize(periodoAmostragemUs);
+  Timer1.attachInterrupt(imprimirLeitura);
 }
 
-const int nLeituras = 1000;
-const int periodoAmostragemUs = 1000;
+
 void loop() {
   // put your main code here, to run repeatedly:
   do {
@@ -39,11 +54,11 @@ void loop() {
   }
   while (!estadoBotao);
 
+  nLeituraAtual = 0;
+  Timer1.start(); //começar coleta de amostras
+  delayMicroseconds(atrasoAplicacaoSinalReferenciaUs);
   aplicarSinalReferencia(1.0f);
-  for (int i = 0; i < nLeituras; i++) {
-    Serial.println(lerSaidaSistema());
-    delayMicroseconds(periodoAmostragemUs);
-  }
-  aplicarSinalReferencia(0.0f);
-
+  delayMicroseconds(nLeituras * periodoAmostragemUs - atrasoAplicacaoSinalReferenciaUs);
+  Timer1.stop(); //para coleta de amostras
+  aplicarSinalReferencia(0.0f); //desligar motor
 }
