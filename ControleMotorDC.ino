f66eeb962d22c273a3d949699d598e5adecca2c3
vbl10 <99830114+vbl10@users.noreply.github.com>
2024-10-22 18:47:07 -0300

    Corrigir erro de tamanho de variáveis de tempo

--- ControleMotorDC.ino
+++ ControleMotorDC.ino
@@ -1,8 +1,8 @@
 #include <TimerOne.h>
 
-const int nLeituras = 1000;
-const int periodoAmostragemUs = 1000;
-const int atrasoAplicacaoSinalReferenciaUs = 10000;
+const long nLeituras = 700;
+const long periodoAmostragemMs = 1;
+const long atrasoAplicacaoSinalReferenciaMs = 10;
 
 void definirDirecao(bool horario) {
   digitalWrite(3, horario);
@@ -40,8 +40,9 @@ void setup() {
   digitalWrite(2,HIGH);              // Coloca o driver TB6612 em operação
   definirDirecao(true);                         // Seta direção de acordo com o valor de "dir"
   Serial.begin(115200);              // Serial initialize
-  
-  Timer1.initialize(periodoAmostragemUs);
+
+  Timer1.initialize(periodoAmostragemMs * 1000L);
+  Timer1.stop();
   Timer1.attachInterrupt(imprimirLeitura);
 }
 
@@ -56,9 +57,9 @@ void loop() {
 
   nLeituraAtual = 0;
   Timer1.start(); //começar coleta de amostras
-  delayMicroseconds(atrasoAplicacaoSinalReferenciaUs);
+  delay(atrasoAplicacaoSinalReferenciaMs);
   aplicarSinalReferencia(1.0f);
-  delayMicroseconds(nLeituras * periodoAmostragemUs - atrasoAplicacaoSinalReferenciaUs);
+  delay(nLeituras * periodoAmostragemMs - atrasoAplicacaoSinalReferenciaMs);
   Timer1.stop(); //para coleta de amostras
   aplicarSinalReferencia(0.0f); //desligar motor
 }
